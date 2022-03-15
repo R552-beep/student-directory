ccab4770cfcdf434d69d8cef87258c6fe4d4e443
Ruzeka <ruzekauddin@hotmail.co.uk>
2022-03-15 22:32:44 +0000

    Added Method

--- directory.rb
+++ directory.rb
@@ -1,14 +1,22 @@
 # frist we print the list of students
 students = [ "Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
 "Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West", "Terminator", " Freddy Krueger", "The Joker", "Joffrey Baratheon", "Norman Bates"]
-# and then print them
+
+def print_header
 puts "The students of Villans Academy"
 puts "-------------"
-students.each do |student|
-    puts student
 end
 
-# finally we print the total number of students 
-puts "Overall, we have #{students.count} great student"
-#it important that print() doesnt add new line characters
+def print(names)
+names.each do |name|
+    puts name
+  end
+end
 
+def print_footer(names)
+puts "Overall, we have #{names.count} great student"
+end
+
+print_header
+print(students)
+print_footer (students)
