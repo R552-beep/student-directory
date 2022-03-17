593d8c771859d9f0ea1387b2f05f13c1161053d7
Ruzeka <ruzekauddin@hotmail.co.uk>
2022-03-17 12:02:43 +0000

    step9 added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,48 +1,46 @@
-def interactive_menu
-  students = []
-  loop do
-    
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit
-  
-  selection = gets.chomp
-  
-  case selection
-  when 1 
-  students = input the students
-  when 2
-    print_header
-    print(students)
-    print_footer(students)
-  when 9
-    exit # this will cause the program to terminate
-  else
-    puts "I don't know what you meant, try again"  
-    
-   end
- end
-end
-
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-# create an empty array
+  # create an empty array
   students = []
-# get the first name
+  # get the first name
   name = gets.chomp
-# while the name is not empty, repeat this code 
+  # while the name is not empty, repeat this code
   while !name.empty? do
-# add the student hash to the array
+    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-# get another name from the user
+    # get another name from the user
     name = gets.chomp
   end
-# return the array of students
+  # return the array of students
   students
-  
+end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
 end
 
 def print_header
@@ -51,17 +49,19 @@ def print_header
 end
 
 def print(students)
-  students.each do |students|
-    puts "#{students[:name]} (#{students[:cohort]} cohort)"
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-#nothing happens until we call the methods
-interactive_menu
 students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
+print_footer(students)
+interactive_menu
